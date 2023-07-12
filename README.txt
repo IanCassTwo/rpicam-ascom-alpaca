d23674c32cfc3c101df68b701b44163118e76101
Ian Cass <ian@wheep.co.uk>
2023-07-12 17:45:47 +0100

    Initial import

---
+++ README.txt
@@ -0,0 +1,2 @@
+pip3 install falcon toml
+apt-get install python3-picamera2 python3-lxml python3-astropy python-rapidjson
---
+++ camerastate.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class CameraState(Enum):
+    IDLE = 0 #CameraIdle At idle state, available to start exposure
+    WAITING = 1 #CameraWaiting Exposure started but waiting (for shutter, trigger, filter wheel, etc.)
+    EXPOSING = 2 #CameraExposing Exposure currently in progress
+    READING = 3 #CameraReading CCD array is being read out (digitized)
+    DOWNLOADING = 4 #CameraDownload Downloading data to PC
+    ERROR = 5 #CameraError Camera error condition serious enough to prevent further operations (connection fail, etc.).
