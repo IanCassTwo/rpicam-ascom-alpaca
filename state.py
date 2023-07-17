ee675c3faeebd46beea65cd1898f1c97abf8a2b6
Ian Cass <ian@wheep.co.uk>
2023-07-17 11:35:54 +0100

    add state object

---
+++ state.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# state.py - Application state
+# access this state from a callback function where we can't pass objects
+##
+# Author:   Ian Cass <ian@wheep.co.uk>
+#
+# Python Compatibility: Requires Python 3.7 or later
+# GitHub: https://github.com/ASCOMInitiative/AlpycaDevice
+#
+# -----------------------------------------------------------------------------
+# MIT License
+#
+# Copyright (c) 2022 Bob Denny
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# -----------------------------------------------------------------------------
+from camerastate import CameraState
+
+class State:
+        def __init__(self):
+                self.camerastate = CameraState.IDLE
+                self.imageReady = False
+                self.last_duration = 0
+                self.job = 0
+                self.gainvalue = 0
+                self.need_restart = False
+                self.SIZE_X = 4056 # FIXME Move this to a camera capabilities class
+                self.SIZE_Y = 3040 # FIXME Move this to a camera capabilities class
+                self.num_x = self.SIZE_X
+                self.num_y = self.SIZE_Y
+                self.start_x = 0
+                self.start_y = 0
+                self.binning = 1
